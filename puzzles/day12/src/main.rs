69a343509dcaddcbfe9c0cebe2a0948f0e5d4db7
oldrich <oldik.kon@gmail.com>
2023-12-11 16:25:37 +0100

    day12 init

---
+++ puzzles/day12/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let input = include_str!("../input_test.txt");
+
+    println!("First part: {}", first_part(input));
+    println!("Second part: {}", second_part(input));
+}
+
+fn first_part(input: &str) -> i32 {
+    0
+}
+
+fn second_part(input: &str) -> i32 {
+    0
+}
