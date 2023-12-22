cdcdd5814b75564c3ef31705e37a4240e0d5e742
oldrich <oldik.kon@gmail.com>
2023-12-22 12:53:20 +0100

    day19 init

---
+++ puzzles/day19/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let input = include_str!("../input_test.txt");
+
+    println!("First part: {}", first_part(input));
+    println!("Second part: {}", second_part(input));
+}
+
+fn first_part(input: &str) -> usize {
+    0
+}
+
+fn second_part(input: &str) -> usize {
+    0
+}
+
+
