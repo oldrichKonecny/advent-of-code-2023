ef61626e3b2a00cc49af9f580932b438095c93d1
oldrich <oldik.kon@gmail.com>
2024-02-28 15:54:31 +0100

    day22 init

---
+++ puzzles/day22/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let input = include_str!("../input.txt");
+
+    println!("First part: {}", first_part(input));
+    println!("Second part: {}", second_part(input));
+}
+
+fn first_part(input: &str) -> usize {
+
+}
+
+fn second_part(input: &str) -> usize {
+
+}
